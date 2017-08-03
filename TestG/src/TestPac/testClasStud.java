38f584646c4fa98f88a96bd480ffb7110a697d49
bhaskar tester <Siva@Siva-PC>
2017-08-03 20:26:45 +0530

    For Student Rep

---
+++ TestG/src/TestPac/testClasStud.java
@@ -0,0 +1,10 @@
+package TestPac;
+
+public class testClasStud {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is for Student rep");
+	}
+
+}
